6d3a61cd4fc4a7b9034e2abd82326015c88c4104
Deivison Nogueira <deivison.nogueira@live.com>
2022-05-02 23:23:04 -0300

    iniciando projeto

---
+++ src/main/java/com/projeto_tads/web/WebApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_tads.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
