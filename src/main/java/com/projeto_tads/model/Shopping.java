86641088942bbaf37e3dc79f9675fbecd2ae07df
Deivison Nogueira <deivison.nogueira@live.com>
2022-05-16 22:29:28 -0300

    alter code shopping

--- src/main/java/com/projeto_tads/model/Shopping.java
+++ src/main/java/com/projeto_tads/model/Shopping.java
@@ -14,29 +14,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 public class Shopping {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  long cnpj;
-
-<<<<<<< HEAD
-  String name;
-  String telephone;
-  // @OneToOne(mappedBy = "Adress")
-  // List<Adress> adress = new ArrayList<Adress>();
-  
-  // @OneToOne (mappedBy = "Restaurant")
-  // List<Restaurant> restaurant = new ArrayList<Restaurant>();
-}
-=======
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     long cnpj;
+
     String name;
     String telephone;
-   // @OneToOne(mappedBy = "Adress")    
-   // List<Adress> adress = new ArrayList<Adress>();
+    // @OneToOne(mappedBy = "Adress")
+    // List<Adress> adress = new ArrayList<Adress>();
 
     // @OneToOne (mappedBy = "Restaurant")
     // List<Restaurant> restaurant = new ArrayList<Restaurant>();
 }
->>>>>>> 8c3b73b3f39da5363a9d1959c018c1669ad72b95
